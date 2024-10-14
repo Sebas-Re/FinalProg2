d83006369213614c67ff0de14aedae9c071f52d5
Sebastián Ré <32151264+Sebas-Re@users.noreply.github.com>
2024-10-14 14:48:53 -0300

    Modif Nombre pantalla

--- app/src/main/java/com/example/finalprog2/fragment/CrearForoFragment.java
+++ app/src/main/java/com/example/finalprog2/fragment/CrearForoFragment.java
@@ -61,6 +61,6 @@ public class CrearForoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_crear_foro, container, false);
+        return inflater.inflate(R.layout.fragment_crear_publicacion, container, false);
     }
 }

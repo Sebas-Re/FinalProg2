733463267a199b49f70b3c858972cbca198b136f
manuel pais <manupais22@gmail.com>
2024-11-16 04:27:13 -0300

    guardado de usuario en foro

--- app/src/main/java/com/example/finalprog2/fragment/VerForoFragment.java
+++ app/src/main/java/com/example/finalprog2/fragment/VerForoFragment.java
@@ -9,11 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import com.example.finalprog2.R;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -35,6 +38,22 @@ public class VerForoFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_ver_foro, container, false);
+        // Configuración del Toolbar
+        Toolbar toolbar = view.findViewById(R.id.custom_toolbar);
+        ((AppCompatActivity) requireActivity()).setSupportActionBar(toolbar);
+
+        // Configuración del título en el Toolbar
+        TextView toolbarTitle = toolbar.findViewById(R.id.toolbar_title);
+        if (toolbarTitle != null) {
+            toolbarTitle.setText("Foro");
+        }
+
+        // Ocultar el nombre de la app
+        toolbar.setTitle("");
+
+        // Botón de retroceso (opcional)
+        ImageButton leftMenuButton = view.findViewById(R.id.left_menu_button);
+        leftMenuButton.setOnClickListener(v -> requireActivity().onBackPressed());
 
         // Vinculación de vistas
         tvForumTitle = view.findViewById(R.id.tv_forum_title);
